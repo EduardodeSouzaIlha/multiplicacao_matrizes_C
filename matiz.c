cd2ff38943b203849a97f463c1392cab2a34ac35
eduardodesouzailha <eduardoilhasouza@gmail.com>
2022-09-28 21:05:38 -0300

    description

--- matiz.c
+++ matiz.c
@@ -6,10 +6,10 @@ int main (){
     puts ("Escreva o tamanho das duas matrizes matriz primeiro linha depois coluna");
     scanf("%d", &rows);
     scanf("%d", &col);
+    if (rows==col){
     int **matriz1 = malloc(sizeof(int*) * rows);
     for (int i = 0; i < col; i++){
         matriz1[i] = malloc(sizeof(int) * col);
-
     }
     int **matriz2 = malloc(sizeof(int*) * rows);
     for (int i = 0; i < col; i++){
@@ -21,13 +21,17 @@ int main (){
         matrizresult[i] = malloc(sizeof(int) * col);
 
     }
+    puts("Escreva os valores da primeira matriz");
     for (size_t matriz1row = 0; matriz1row< rows; matriz1row++){
         for(size_t matriz1col=0; matriz1col< col; matriz1col++){
+        printf("Escreva o valor da linha %d coluna %d\n", matriz1row+1, matriz1col+1);
         scanf("%d", &matriz1[matriz1row][matriz1col]);    
         }
     }
+    puts("Escreva os valores da segunda matriz");
     for (size_t matriz2row = 0; matriz2row < rows; matriz2row++){
         for (size_t matriz2col = 0; matriz2col < col; matriz2col++){
+         printf("Escreva o valor da linha %d coluna %d\n", matriz2row+1, matriz2col+1);
         scanf("%d", &matriz2[matriz2row][matriz2col]);  
         }
     }
@@ -35,16 +39,12 @@ int main (){
         
         for (size_t matriz_result_col = 0; matriz_result_col<col; matriz_result_col++){
             for (size_t matriz1col = 0; matriz1col< col; matriz1col++){
-                for( row ; row< rows; row++){
+                while(row< rows){
                     if(row==0){
                     matrizresult[matriz_result_row][matriz_result_col]=matriz1[matriz_result_row][matriz1col]*matriz2[row][matriz_result_col];
-                   
-                    
                     }
                     else{
                     matrizresult[matriz_result_row][matriz_result_col]+=matriz1[matriz_result_row][matriz1col]*matriz2[row][matriz_result_col];
-                  
-
                     }
                     row++;
                     if (row==rows){
@@ -59,11 +59,12 @@ int main (){
     
                      
     for (size_t matriz_result_row = 0; matriz_result_row<rows; matriz_result_row++){
-  
+        printf("\n");
         for (size_t matriz_result_col = 0; matriz_result_col<col; matriz_result_col++){
            
-            printf("%d\n", matrizresult[matriz_result_row][matriz_result_col]);
+            printf("%d\t", matrizresult[matriz_result_row][matriz_result_col]);
 
         }
+    }
     }
 }
